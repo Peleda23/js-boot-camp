20f4e7b9ad42d263cf98fd75706a114f1ed96113
Peleda23 <murinas.tomas@gmail.com>
2018-12-07 05:37:03 +0200

    testas

--- arrays/todos.js
+++ arrays/todos.js
@@ -14,13 +14,12 @@ const todos = [{
 
 const findTodo = function(todos, todosText){
   return todos.find(function(todo, index){
-    console.log(index);
+    
     return todo.text.toLowerCase() === todosText.toLowerCase();
   });
-  
-  
 };
 
 const todo = findTodo(todos, 'find new job');
 console.log(todo);
 console.log(todos);
+console.log(todo);
