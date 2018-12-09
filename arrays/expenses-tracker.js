378165712f64d944c15707fe8f5f4b8791f000a4
Peleda23 <kosmosas23@gmail.com>
2018-12-09 19:58:07 +0200

    adde one function

--- arrays/expenses-tracker.js
+++ arrays/expenses-tracker.js
@@ -7,6 +7,23 @@ const account = {
 // addExpenses -> description, amount.
 // getAccountSummary -> total up all expenses -> Tomas Murinas has $1267 in expenses.
 
-account.addExpenses('Rent', 950);
-account.addExpenses('Coffee', 2);
-console.log(account.getAccountSummary());
+const addExpenses = function(description, amount){
+    return account.expenses.push({description, amount});
+};
+
+const getAccountSummary = function(account){
+    account.forEach(function(acc, index){
+        let test = expenses.amount;
+        return test;
+    });
+};
+
+
+
+
+
+
+addExpenses('Rent', 950);
+addExpenses('Coffee', 2);
+console.log(account);
+console.log(getAccountSummary(account));
