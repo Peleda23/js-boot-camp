fa1d3c7092f02da809ab22735dbc357318b14ea6
Peleda23 <kosmosas23@gmail.com>
2018-12-10 06:50:09 +0200

    Worked on expensive tracker app

--- arrays/expenses-tracker.js
+++ arrays/expenses-tracker.js
@@ -1,29 +1,45 @@
 const account = {
     name: 'Tomas Murinas',
-    expenses: []
+    expenses: [],
+    income: [],
+    addExpenses: function(description, amount){
+        this.expenses.push({
+            description: description,
+            amount: amount
+        });
+    },
+    addIncome: function(description, amount){
+        this.income.push({
+            description: description,
+            amount: amount
+        });
+    },
+    getAccountSummary: function(){
+        let totalExpenses = 0;
+        let totalIncome = 0;
+        let total = 0;
+        this.expenses.forEach(function(expenses){
+            totalExpenses = totalExpenses + expenses.amount;
+        });
+        
+        this.income.forEach(function(income){
+            totalIncome = totalIncome + income.amount;
+        });
+
+        total = totalIncome - totalExpenses;
+
+        return `${this.name} has a balance of $${total}. $${totalIncome} in income. $${totalExpenses} in expenses.`;
+    }
 };      
 
-// Expenses -> description, amount.
-// addExpenses -> description, amount.
-// getAccountSummary -> total up all expenses -> Tomas Murinas has $1267 in expenses.
+// 1. add income array to account.
+// 2. addIncome method -> description, amount.
+// 3. Tweak getAccountSummary.
 
-const addExpenses = function(description, amount){
-    return account.expenses.push({description, amount});
-};
-
-const getAccountSummary = function(account){
-    account.forEach(function(acc, index){
-        let test = expenses.amount;
-        return test;
-    });
-};
+// Tomas Murinas has a balance of 10$. 100$ in income. 90$ in expenses.
 
 
-
-
-
-
-addExpenses('Rent', 950);
-addExpenses('Coffee', 2);
-console.log(account);
-console.log(getAccountSummary(account));
+account.addExpenses('Rent', 950);
+account.addExpenses('Coffee', 2);
+account.addIncome('Job', 1000);
+console.log(account.getAccountSummary());
