1f12065d81d0aac16e2fa30002145a230281432b
Peleda23 <kosmosas23@gmail.com>
2018-12-13 21:24:26 +0200

    working with inputs

--- note-app/notes-app.js
+++ note-app/notes-app.js
@@ -13,19 +13,17 @@ const notes = [{
     searchText: ''
   };
 
-//* Funkcija filtruoja ir iskelia i ekrana zinutes.
+// * Funkcija filtruoja ir iskelia i ekrana zinutes.
 
   const renderNotes = function(notes, filters){
     const filteredNotes = notes.filter(function(note){
       return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
     });
 
-    //* Istrina visa kita nereikalinga teksta ir palieka tik naujai isfiltruota.
-
+    // * Istrina visa kita nereikalinga teksta ir palieka tik naujai isfiltruota.
     document.querySelector('#notes').innerHTML = '';
 
-    //* Funkcija isfiltruotas zinutes iskelia i ekrana.
-    
+    // * Funkcija isfiltruotas zinutes iskelia i ekrana.
     filteredNotes.forEach(function(note){
       const noteEl = document.createElement('p');
       noteEl.textContent = note.title;
@@ -39,13 +37,18 @@ const notes = [{
     e.target.textContent = 'The button was clicked';
   });
 
-  document.querySelector('#remove-all').addEventListener('click', function(){
-    document.querySelectorAll('.note').forEach(function(note){
-      note.remove();
-    });
-  });
+  
 
   document.querySelector('#search-text').addEventListener('input', function(e){
     filters.searchText = e.target.value;
     renderNotes(notes, filters);
+  });
+// * Sujungiam su forma.
+  document.querySelector('#name-form').addEventListener('submit', function(e){
+    // * Nustatom, kad forma nedarytu standartiskai.
+    e.preventDefault();
+    // * Kadangi formoje gali buti daug inputu tai rasom(element.firstName(pagal duota varda)) taip ji issikriame.
+    console.log(e.target.elements.firstName.value);
+    // * Paspaudus submit migtuka istrina inpute parasyta zodi.
+    e.target.elements.firstName.value = '';
   });
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,31 +15,49 @@ const todos = [{
         completed: true
     }];
 
+// * Filtruoto teksto objektas.
+
+const filters = {
+    searchText: ''
+};
+
+// * Tikrina filters objekto ir esamo todos objekto zodzius.
+const renderTodos = function(todos, filters) {
+    const filteredTodos = todos.filter(function(todo){
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+    });
+
+    // * filtruoja nepadarytus todo.
+    const incompleteTodos = filteredTodos.filter(function(todo){
+        return !todo.completed;
+    });
+    
+    // * Isvalo lauka kad nesikratotu vienoda info.
+    document.querySelector('#todos').innerHTML = '';
+
+    // * Pateikia esamu nepadarytu todo skaiciu.
+    const summary = document.createElement('h2');
+    summary.textContent = `You have ${incompleteTodos.length} todos left`;
+    document.querySelector('#todos').appendChild(summary);
+    
+    // * Daro loopa per esama objekta ir iskelia visus todo i teksta p pagrindu.
+    filteredTodos.forEach(function(todo){
+        const p = document.createElement('p');
+        p.textContent = todo.text;
+        document.querySelector('#todos').appendChild(p);
+    });
+};
+
+// * Iskvieca funkcija kad parasytu esamus todo.
+renderTodos(todos, filters);
 
 
 
-const incompleteTodos = todos.filter(function(todo){
-    return !todo.completed;
-});
 
-const summary = document.createElement('h2');
-summary.textContent = `You have ${incompleteTodos.length} todos left`;
-document.querySelector('body').appendChild(summary);
+// * Surenka parasytus zodzius ir sukelia i filters objekta.
+document.querySelector('#search-text').addEventListener('input', function(e){
+    filters.searchText = e.target.value;
 
-todos.forEach(function(todo){
-    const p = document.createElement('p');
-    p.textContent = todo.text;
-    document.querySelector('body').appendChild(p);
-});
-
-// Listen for new todo creation.
-
-document.querySelector('#add-todo').addEventListener('click', function(e){
-    console.log('my message');
-});    
-
-// Listen fot todo text change.
-
-document.querySelector("#new-todo-text").addEventListener('input', function(e){
-    console.log(e.target.value);
+    // * Iskviecia funkcija kai buna panaudotas inputas. 
+    renderTodos(todos, filters);
 });
