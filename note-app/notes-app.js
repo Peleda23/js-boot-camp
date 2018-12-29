114e23ecac78c7329287092b09072d747dbec98b
Peleda23 <kosmosas23@gmail.com>
2018-12-29 18:37:44 +0200

    come back from break

--- note-app/notes-app.js
+++ note-app/notes-app.js
@@ -1,7 +1,8 @@
 let notes = getSavedNotes();
 
   const filters = {
-    searchText: ''
+    searchText: '',
+    sortBy: 'byEdited'
   };
 
   renderNotes(notes, filters);
@@ -32,7 +33,8 @@ let notes = getSavedNotes();
   });
 
   document.querySelector('#filter-by').addEventListener('change', function(e){
-    console.log(e.target.value);
+    filters.sortBy = e.target.value;
+    renderNotes(notes, filters);
   });
 
   window.addEventListener('storage', function (e) {
