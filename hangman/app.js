56d1b3515a26aa23b72b267b3fab672ccc1a7a9d
Peleda23 <kosmosas23@gmail.com>
2019-01-18 06:27:54 +0200

    adde another API

--- hangman/app.js
+++ hangman/app.js
@@ -6,7 +6,7 @@ puzzleEl.textContent = game1.puzzle;
 guessEl.textContent = game1.statusMessage;
 
 
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode);
     game1.makeGuess(guess);
     puzzleEl.textContent = game1.puzzle;
@@ -17,11 +17,29 @@ window.addEventListener('keypress', function (e) {
 const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4) {
+    if (e.target.readyState === 4 && e.target.status === 200) {
         const data = JSON.parse(e.target.responseText);
         console.log(data);
+    } else if (e.target.readyState === 4) {
+        console.log('An error has taken place');
     }
 });
 
-request.open('GET', 'http://puzzle.mead.io/puzzle');
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3');
 request.send();
+
+const countryCode = 'LT';
+const countryRequest = new XMLHttpRequest();
+
+countryRequest.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText);
+        const country = data.find((country) => country.alpha2Code === countryCode);
+        console.log(country.name);
+    } else if (e.target.readyState === 4) {
+        console.log('Error Error');
+    }
+});
+
+countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
+countryRequest.send();
