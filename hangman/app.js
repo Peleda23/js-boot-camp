226dbe6ed7927c923fe91936d597236de80ac31e
Peleda23 <kosmosas23@gmail.com>
2019-01-21 06:30:33 +0200

    added call back function

--- hangman/app.js
+++ hangman/app.js
@@ -13,33 +13,29 @@ window.addEventListener('keypress', (e) => {
     guessEl.textContent = game1.statusMessage;
 });
 
+getPuzzle((error, puzzle) => {
+    if (error) {
+        console.log(`Error ${error}`);
+    } else {
+        console.log(puzzle);
+    }
+});
+
 // Make an Http request
-const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText);
-        console.log(data);
-    } else if (e.target.readyState === 4) {
-        console.log('An error has taken place');
-    }
-});
 
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3');
-request.send();
+// const countryCode = 'LT';
+// const countryRequest = new XMLHttpRequest();
 
-const countryCode = 'LT';
-const countryRequest = new XMLHttpRequest();
+// countryRequest.addEventListener('readystatechange', (e) => {
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText);
+//         const country = data.find((country) => country.alpha2Code === countryCode);
+//         console.log(country.name);
+//     } else if (e.target.readyState === 4) {
+//         console.log('Error Error');
+//     }
+// });
 
-countryRequest.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText);
-        const country = data.find((country) => country.alpha2Code === countryCode);
-        console.log(country.name);
-    } else if (e.target.readyState === 4) {
-        console.log('Error Error');
-    }
-});
-
-countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
-countryRequest.send();
+// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
+// countryRequest.send();
